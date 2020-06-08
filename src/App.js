e4589fd5a0634f98285d7e5a6e597232b2aa8398
Sukriti Bhardwaj <sukriti@lawn-128-61-48-150.lawn.gatech.edu>
2020-06-08 14:41:03 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
-
+import { Layout, Header, Navigation, Drawer,Content} from 'react-mdl';
+import {Link} from 'react-router-dom';
+import Main from './components/main'
 function App() {
   return (
-    <div>
-      <h1>Hellooo boo boo</h1>
-    </div>
+    <div className="demo-big-content">
+    <Layout>
+        <Header className = "header-col" title="Title" scroll>
+            <Navigation>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/Experience">Experience</Link>
+                <Link to="/skills">Skills</Link>
+                <Link to="/project">Projects</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/Experience">Experience</Link>
+                <Link to="/skills">Skills</Link>
+                <Link to="/project">Projects</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
   );
 }
 
---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import home from './home';
+import skills from './skills';
+import aboutme from './aboutme';
+import experience from './experience';
+import project from './project';
+const Main = () => (
+    <Switch>
+        <Route exact path = "/" component = {home} />
+        <Route path = "/skills" component = {skills} />
+        <Route path = "/aboutme" component = {aboutme}/>
+        <Route path = "/experience" component = {experience}/>
+        <Route path = "/project" component = {project} />
+    </Switch>
+)
+export default Main;
